75995d5c26536f11c8b20a7934771555ac2b4ad3
Walter van den Houten <waltervdhouten@gmail.com>
2024-02-21 23:41:57 +0200

    Added admin management page

---
+++ src/Sportle/Sportle.Web/Areas/Admin/Controllers/AdminsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sportle.Web.Areas.Admin.Models;
+using Sportle.Web.Data;
+
+namespace Sportle.Web.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize(Roles = "Admin")]
+    public class AdminsController : Controller
+    {
+        private readonly SportleDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminsController(SportleDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new AdminsViewModel()
+            {
+                Users = await _context.Users.ToListAsync(),
+                Admins = await _userManager.GetUsersInRoleAsync("Admin")
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(AdminsViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.UserId.ToString());
+                if (user is not null)
+                {
+                    switch (model.Action)
+                    {
+                        case "Add":
+                            await _userManager.AddToRoleAsync(user, "Admin");
+                            break;
+                        case "Remove":
+                            if (user.NormalizedUserName?.ToUpperInvariant() == "PATIENT-ZERO")
+                                break;
+
+                            await _userManager.RemoveFromRoleAsync(user, "Admin");
+                            break;
+                    }
+                }
+            }
+
+            model.Users = await _context.Users.ToListAsync();
+            model.Admins = await _userManager.GetUsersInRoleAsync("Admin");
+
+            return View(model);
+        }
+    }
+}
---
+++ src/Sportle/Sportle.Web/Areas/Admin/Models/AdminsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+
+namespace Sportle.Web.Areas.Admin.Models
+{
+    public class AdminsViewModel
+    {
+        public IList<IdentityUser> Users { get; set; } = [];
+
+        public IList<IdentityUser> Admins { get; set; } = [];
+
+        [DisplayName("User")]
+        public Guid UserId { get; set; }
+
+        public string? Action { get; set; }
+    }
+}
