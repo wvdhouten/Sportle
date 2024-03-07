c1fb00cea30bf3fe32f68628d4071b0645c0f535
Walter van den Houten <waltervdhouten@gmail.com>
2024-03-07 18:35:24 +0200

    added PWA

--- src/Sportle/Sportle.Web/wwwroot/js/site.js
+++ src/Sportle/Sportle.Web/wwwroot/js/site.js
@@ -1,4 +1,6 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js').then(function () { console.log('Service worker registered'); });;
+};
